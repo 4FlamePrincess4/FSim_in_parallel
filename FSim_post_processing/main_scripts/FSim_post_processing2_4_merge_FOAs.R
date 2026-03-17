31b94e140d5315722094887bcb6d6a5d9fab3499
Laurel Sindewald <laurelsindewald@gmail.com>
2026-03-17 16:13:02 -0600

    Update FSim_post_processing2_4_merge_FOAs.R
    More specific regex to grep the right directory

--- FSim_post_processing/main_scripts/FSim_post_processing2_4_merge_FOAs.R
+++ FSim_post_processing/main_scripts/FSim_post_processing2_4_merge_FOAs.R
@@ -60,7 +60,7 @@ run_label <- paste0(opt$scenario, "_", opt$run_timepoint)
 
 # Locate subdirectories containing the scenario in their names
 target_dirs <- list.dirs(path = wd, recursive = FALSE, full.names = TRUE) %>%
-  .[grepl(run_label, .)]
+  .[grepl(paste0(run_label, "$"), .)]
 if (length(target_dirs) == 0) {
   stop("No matching directories found for the specified scenario.")
 }
